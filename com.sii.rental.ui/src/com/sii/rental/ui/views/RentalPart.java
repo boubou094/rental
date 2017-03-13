8e7392595c8133caa284905f3373752989e3820a
VAELIA8 <VAELIA8@10.131.100.59>
2017-03-13 17:01:37 +0100

    Add setRental in RentalPart.java

--- com.sii.rental.ui/src/com/sii/rental/ui/views/RentalPart.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/views/RentalPart.java
@@ -3,39 +3,57 @@ package com.sii.rental.ui.views;
 
 import javax.annotation.PostConstruct;
 
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.e4.ui.di.Focus;
+
+import com.opcoach.training.rental.Rental;
+import com.sii.rental.ecore.RentalCoreActivator;
 
 public class RentalPart {
 
 	private Label rentedObjectLabel, customerNameLabel;
 
+	public void setRental(Rental rental) {
+		rentedObjectLabel.setText(rental.getRentedObject().getName());
+		customerNameLabel.setText("Loué à : " + rental.getCustomer().getLastName() + " " + rental.getCustomer().getFirstName());
+	}
+	
 	@PostConstruct
 	public void createUi(Composite parent) {
-		parent.setLayout(new GridLayout(1, false));
+		parent.setLayout(GridLayoutFactory
+				.fillDefaults()
+				.numColumns(1)
+				.equalWidth(false)
+				.create());
 		
 		Group infoGroup = new Group(parent, SWT.NONE);
 		infoGroup.setText("Information");
-		infoGroup.setLayout(new GridLayout(2, false));
+		infoGroup.setLayout(GridLayoutFactory
+				.fillDefaults()
+				.numColumns(2)
+				.equalWidth(false)
+				.create());
 		
 		rentedObjectLabel = new Label(infoGroup, SWT.BORDER);
 		GridData gd = new GridData();
 		gd.horizontalSpan = 2;
 		gd.horizontalAlignment = SWT.FILL;
 		rentedObjectLabel.setLayoutData(gd);
-		rentedObjectLabel.setText("Perceuse Electrique");
+		//rentedObjectLabel.setText("Perceuse Electrique");
 		
 		customerNameLabel = new Label(infoGroup, SWT.BORDER);
 		GridData gdName = new GridData();
 		gdName.horizontalSpan = 2;
 		gdName.horizontalAlignment = SWT.FILL;
 		customerNameLabel.setLayoutData(gdName);
-		customerNameLabel.setText("Loué à : John Wayne");
+		//customerNameLabel.setText("Loué à : John Wayne");
+		
+		setRental(RentalCoreActivator.getAgency().getRentals().get(0));
 	}
 
 	@Focus
