7a16a776f5f335b68551a0682ae521c6857c1c08
VAELIA8 <VAELIA8@10.131.100.59>
2017-03-14 14:55:37 +0100

    Add agency categories

--- com.sii.rental.ui/src/com/sii/rental/ui/RentalProvider.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/RentalProvider.java
@@ -1,6 +1,7 @@
 package com.sii.rental.ui;
 
 import java.util.Collection;
+import java.util.function.Supplier;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.viewers.ITreeContentProvider;
@@ -8,9 +9,36 @@ import org.eclipse.jface.viewers.LabelProvider;
 
 import com.opcoach.training.rental.Customer;
 import com.opcoach.training.rental.RentalAgency;
+import com.opcoach.training.rental.RentalObject;
 
 public class RentalProvider extends LabelProvider implements ITreeContentProvider {
 
+	class AgencyCategory {
+		private final String name;
+		private final RentalAgency agency;
+		private final Supplier<Object[]> childrenSuplier;
+		
+		AgencyCategory(String name, RentalAgency agency, Supplier<Object[]> childrenSuplier) {
+			this.name = name;
+			this.agency = agency;
+			this.childrenSuplier = childrenSuplier;
+		}
+		
+		RentalAgency getRentalAgency() {
+			return agency;
+		}
+		
+		public Object[] getchildren() {
+			return childrenSuplier.get();
+		}
+
+		@Override
+		public String toString() {
+			return name;
+		}
+		
+	}
+	
 	@Override
 	public String getText(Object element) {
 		if (RentalAgency.class.isInstance(element)) {
@@ -19,7 +47,14 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 		} else if (Customer.class.isInstance(element)) {
 			final Customer customer = Customer.class.cast(element);
 			return customer.getDisplayName();
+		} else if (AgencyCategory.class.isInstance(element)) {
+			final AgencyCategory customer = AgencyCategory.class.cast(element);
+			return customer.toString();
+		} else if (RentalObject.class.isInstance(element)) {
+			final RentalObject rentalObject = RentalObject.class.cast(element);
+			return rentalObject.getName();
 		}
+		
 
 		return super.getText(element);
 	}
@@ -37,7 +72,14 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 	public Object[] getChildren(Object parentElement) {
 		if (RentalAgency.class.isInstance(parentElement)) {
 			final RentalAgency agency = RentalAgency.class.cast(parentElement);
-			return agency.getCustomers().toArray();
+			AgencyCategory[] agencyCategories = new AgencyCategory[] {
+				new AgencyCategory("Customers", agency, () -> agency.getCustomers().toArray()),
+				new AgencyCategory("Locations", agency, () -> agency.getRentals().toArray()),
+				new AgencyCategory("Object à louer", agency, () -> agency.getObjectsToRent().toArray())
+			};
+			return agencyCategories;
+		} else if (AgencyCategory.class.isInstance(parentElement)) {
+			return AgencyCategory.class.cast(parentElement).getchildren();
 		}
 
 		return new Object[0];
@@ -60,6 +102,8 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 		if (RentalAgency.class.isInstance(element)) {
 			final RentalAgency agency = RentalAgency.class.cast(element);
 			return !agency.getCustomers().isEmpty();
+		} else if (AgencyCategory.class.isInstance(element)) {
+			return true;
 		}
 
 		return false;
