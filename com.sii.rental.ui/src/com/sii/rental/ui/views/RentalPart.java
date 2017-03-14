b3f774a2d73753bb73625ae6037ab5484906b0b6
VAELIA8 <VAELIA8@10.131.100.59>
2017-03-14 09:33:50 +0100

    Add start and end date

--- com.sii.rental.ui/src/com/sii/rental/ui/views/RentalPart.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/views/RentalPart.java
@@ -13,14 +13,21 @@ import org.eclipse.swt.widgets.Label;
 
 import com.opcoach.training.rental.Rental;
 import com.sii.rental.ecore.RentalCoreActivator;
+import org.eclipse.swt.layout.GridLayout;
 
 public class RentalPart {
 
 	private Label rentedObjectLabel, customerNameLabel;
+	private Group rentalLocationDates;
+	private Label rentalDateToLabel;
+	private Label rentalDateToEdit;
+	private Label rentalDateFromEdit;
 
 	public void setRental(Rental rental) {
 		rentedObjectLabel.setText(rental.getRentedObject().getName());
 		customerNameLabel.setText("Loué à : " + rental.getCustomer().getLastName() + " " + rental.getCustomer().getFirstName());
+		rentalDateFromEdit.setText(rental.getStartDate().toString());
+		rentalDateToEdit.setText(rental.getEndDate().toString());
 	}
 	
 	@PostConstruct
@@ -32,6 +39,7 @@ public class RentalPart {
 				.create());
 		
 		Group infoGroup = new Group(parent, SWT.NONE);
+		infoGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		infoGroup.setText("Information");
 		infoGroup.setLayout(GridLayoutFactory
 				.fillDefaults()
@@ -41,6 +49,7 @@ public class RentalPart {
 		
 		rentedObjectLabel = new Label(infoGroup, SWT.BORDER);
 		GridData gd = new GridData();
+		gd.grabExcessHorizontalSpace = true;
 		gd.horizontalSpan = 2;
 		gd.horizontalAlignment = SWT.FILL;
 		rentedObjectLabel.setLayoutData(gd);
@@ -48,9 +57,33 @@ public class RentalPart {
 		
 		customerNameLabel = new Label(infoGroup, SWT.BORDER);
 		GridData gdName = new GridData();
+		gdName.grabExcessHorizontalSpace = true;
 		gdName.horizontalSpan = 2;
 		gdName.horizontalAlignment = SWT.FILL;
 		customerNameLabel.setLayoutData(gdName);
+		
+		rentalLocationDates = new Group(parent, SWT.NONE);
+		rentalLocationDates.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		rentalLocationDates.setText("Dates de location");
+		rentalLocationDates.setLayout(new GridLayout(2, false));
+		
+		Label rentalDateFromLabel = new Label(rentalLocationDates, SWT.NONE);
+		rentalDateFromLabel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		rentalDateFromLabel.setText("Du :");
+		
+		rentalDateFromEdit = new Label(rentalLocationDates, SWT.NONE);
+		rentalDateFromEdit.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
+		rentalDateFromEdit.setSize(55, 15);
+		rentalDateFromEdit.setText("xx/xx/xxxx");
+		
+		rentalDateToLabel = new Label(rentalLocationDates, SWT.NONE);
+		rentalDateToLabel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		rentalDateToLabel.setSize(19, 15);
+		rentalDateToLabel.setText("Au :");
+		
+		rentalDateToEdit = new Label(rentalLocationDates, SWT.NONE);
+		rentalDateToEdit.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
+		rentalDateToEdit.setText("xx/xx/xxxx");
 		//customerNameLabel.setText("Loué à : John Wayne");
 		
 		setRental(RentalCoreActivator.getAgency().getRentals().get(0));
@@ -60,5 +93,4 @@ public class RentalPart {
 	public void onFocus() {
 		rentedObjectLabel.setFocus();
 	}
-
 }
