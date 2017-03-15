8e9ab813c27481bb9e08de977cbd26dfdf3a859c
VAELIA8 <VAELIA8@10.131.100.59>
2017-03-15 09:31:36 +0100

    Add multiple selections customer

--- com.sii.rental.ui/src/com/sii/rental/ui/views/RentalPart.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/views/RentalPart.java
@@ -1,12 +1,17 @@
 
 package com.sii.rental.ui.views;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
 import org.eclipse.jface.layout.GridLayoutFactory;
@@ -17,6 +22,7 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 
+import com.opcoach.training.rental.Customer;
 import com.opcoach.training.rental.Rental;
 import com.opcoach.training.rental.RentalAgency;
 
@@ -27,6 +33,7 @@ public class RentalPart {
 	private Label rentalDateToLabel;
 	private Label rentalDateToEdit;
 	private Label rentalDateFromEdit;
+	private String partId;
 
 	public void setRental(Rental rental) {
 		if (java.util.Optional.ofNullable(rental).isPresent()) {
@@ -37,11 +44,15 @@ public class RentalPart {
 			rentalDateToEdit.setText(rental.getEndDate().toString());
 		} else {
 			rentedObjectLabel.setText("");
-			customerNameLabel.setText(
-					"Loué à : ");
+			customerNameLabel.setText("Loué à : ");
 			rentalDateFromEdit.setText("");
 			rentalDateToEdit.setText("");
 		}
+	}
+
+	private void setCustomer(Set<Customer> customers) {
+		customerNameLabel.setText("Loué à : "
+				+ customers.stream().map(Customer::getDisplayName).reduce((n1, n2) -> n1 + "; " + n2).orElse(""));
 	}
 
 	@SuppressWarnings("unused")
@@ -49,7 +60,8 @@ public class RentalPart {
 	private ESelectionService selectionService;
 
 	@Inject
-	public RentalPart(Composite parent, RentalAgency rentalAgency) {
+	public RentalPart(MPart part, Composite parent, RentalAgency rentalAgency) {
+		partId = part.getElementId();
 		parent.setLayout(GridLayoutFactory.fillDefaults().numColumns(1).equalWidth(false).create());
 
 		Group infoGroup = new Group(parent, SWT.NONE);
@@ -104,16 +116,36 @@ public class RentalPart {
 		rentedObjectLabel.setFocus();
 	}
 
-	@Inject	@Optional	
-	public void receiveSelection(@Named(IServiceConstants.ACTIVE_SELECTION) Rental rental) {
-		this.setRental(rental);
+	@Inject
+	@Optional
+	public void receiveSelection(@Named(IServiceConstants.ACTIVE_SELECTION) Rental rental,
+			@Named(IServiceConstants.ACTIVE_PART) MPart part) {
+		if (java.util.Optional.ofNullable(part)
+			.filter((p) -> 0 != partId.compareTo(part.getElementId()))
+			.isPresent()) {
+			this.setRental(rental);
+		}
+	}
+	
+	@Inject
+	@Optional
+	public void receiveSelection(@Named(IServiceConstants.ACTIVE_SELECTION) Customer customer,
+			@Named(IServiceConstants.ACTIVE_PART) MPart part) {
+		if (java.util.Optional.ofNullable(part)
+			.filter((p) -> 0 != partId.compareTo(part.getElementId()))
+			.isPresent()) {
+			this.setCustomer(java.util.Optional.ofNullable(customer)
+					.map((c) -> Stream.of(c))
+					.orElse(Stream.empty())
+					.collect(Collectors.toSet()));
+		}
 	}
 
-	
-	@Inject @Optional public void
-	selectCustomers(@Named(IServiceConstants.ACTIVE_SELECTION) Object[] objects) {
-		/*Arrays.stream(objects).filter(Customer.class::isInstance)
-	 .map(this::setC);*/
+	@Inject
+	@Optional
+	public void selectCustomers(@Named(IServiceConstants.ACTIVE_SELECTION) Object[] objects) {
+		java.util.Optional.ofNullable(objects).ifPresent((customers) -> this.setCustomer(Arrays.stream(customers)
+				.filter(Customer.class::isInstance).map(Customer.class::cast).collect(Collectors.toSet())));
 	}
-	 
+
 }
